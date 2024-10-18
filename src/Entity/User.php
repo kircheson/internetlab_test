7ad08a064a975c288368e63cedf6f73218901570
kircheson <kirill.kotlyaren@mail.ru>
2024-10-18 15:14:51 +0500

    Первоначальная структура (Сущность, Репозиторий, DTO, миграция)

---
+++ src/Entity/User.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+#[ORM\Table(name: 'users')]
+class User
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 180, unique: true)]
+    #[Assert\NotBlank(message: "Email не должен быть пустым")]
+    #[Assert\Email(message: "The email '{{ value }}' is not a valid email.")]
+    private string $email;
+
+    #[ORM\Column]
+    #[Assert\NotBlank(message: "Password не должен быть пустым")]
+    #[Assert\Length(min: 6, minMessage: "Password must be at least {{ limit }} characters long.")]
+    private string $password;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+        return $this;
+    }
+
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+        return $this;
+    }
+}
---
+++ src/Model/UserDTO.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Model;
+
+class UserDTO
+{
+    public string $email;
+    public string $password;
+
+    public function __construct(string $email, string $password)
+    {
+        $this->email = $email;
+        $this->password = $password;
+    }
+}
