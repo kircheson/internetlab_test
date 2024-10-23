783e9ebc77fa060f8474a0ba6718414f803e9bcf
kircheson <kirill.kotlyaren@mail.ru>
2024-10-24 01:19:44 +0500

    Реализовал фикстуры циклом, библиотекой Faker

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,23 +5,18 @@ namespace App\DataFixtures;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        $users = [
-            ['email' => 'user1@example.com', 'password' => 'password1'],
-            ['email' => 'user2@example.com', 'password' => 'password2'],
-            ['email' => 'user3@example.com', 'password' => 'password3'],
-            ['email' => 'user4@example.com', 'password' => 'password4'],
-            ['email' => 'user5@example.com', 'password' => 'password5'],
-        ];
+        $faker = Factory::create();
 
-        foreach ($users as $userData) {
+        for ($i = 0; $i < 10; $i++) {
             $user = new User();
-            $user->setEmail($userData['email']);
-            $user->setPassword(password_hash($userData['password'], PASSWORD_BCRYPT));
+            $user->setEmail($faker->unique()->email);
+            $user->setPassword(password_hash($faker->password, PASSWORD_BCRYPT));
 
             $manager->persist($user);
         }
